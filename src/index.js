6152dc53a3289467920c5281071c58e04887767a
mahmedsaleem1 <mahmedsaleem786@gmail.com>
2024-12-25 11:54:56 +0500

    utils

--- src/index.js
+++ src/index.js
@@ -9,26 +9,13 @@ dotenv.config(
 );
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port ${process.env.PORT} `)
+    })
+})
+.catch((error) => {
+    console.log(`Database connection Failed : ${error} `)
+})
 
-
-/*
-import express from "express";
-const app = express();
-
-;( async () => {
-    try {
-        mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("Error connecting to database");
-            throw error;
-        })
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}) () */
-// The code snippet above is a refactored version of the code snippet in src/db/index.js.
