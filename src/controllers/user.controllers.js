9352620e80b4ba29beb4217cb9a0aa57a444bb5d
mahmedsaleem1 <mahmedsaleem786@gmail.com>
2024-12-28 19:32:24 +0500

    user Registration contoller and bug fixes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,59 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js"
+import { User } from "../models/user.models.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {ApiResponse} from "../Utils/ApiResponse.js"
+
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "Hello from registerUser",
+    const {username, email, password, fullname} = req.body
+    
+    if (
+      [username, email, password, fullname].some((field) => field?.trim() === "") 
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existUser = User.findOne({ username })
+
+    if (existUser) {
+        throw new ApiError(401, "Username already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+      throw new ApiError(400, "Avatar is required");
+    }
+
+    const userM = await User.create({
+      fullname,
+      avatar: avatar.url,
+      coverImage: coverImage?.url || "",
+      email,
+      password,
+      username : username.toLowerCase(),
     })
-})
+
+    const createdUser = await User.findById(User._id).select(
+      "-password -refreshToken" // what not to have?
+    )
+
+    if (!createdUser) {
+      throw new ApiError(500, "User is not created please try again")
+    }
+
+    return res.status(201).json(
+      new ApiResponse(200, createdUser, "User Registered Successfully")
+    )
+  })
 
 export {registerUser}
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 
- const storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, "./public/temp")
     },
@@ -9,4 +9,4 @@ import multer from "multer";
     }
  })
 
- export const upload = multer({ storage })
+export const upload = multer({ storage })
